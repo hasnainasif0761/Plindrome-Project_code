b8f9be9d65895250cc69c8b6a85f70615077ac7a
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-12 23:58:36 +0500

    'First-Change'

---
+++ script.js
@@ -0,0 +1,180 @@
+// var heading = document.getElementById('head');
+
+// console.log(heading.firstChild);
+
+// var heading = document.getElementById('head');
+
+// console.log(heading.lastChild);
+
+
+// var kidNode = document.getElementById('div2');
+// var pNode = kidNode.parentNode
+
+// pNode.style.border='1px solid red'
+
+// console.log(pNode);
+
+
+// var firstEl = document.getElementById('div3');
+
+// var PrevSibl = firstEl.previousSibling
+// var NextSibl = firstEl.nextSibling
+
+// console.log(PrevSibl);
+// console.log(NextSibl);
+
+
+
+// var nodeVal = document.getElementById('head');
+
+//  let a = nodeVal.firstChild.nodeValue
+
+// console.log(a);
+
+
+
+// let liElement = document.getElementsByTagName('li');
+
+// let howMany = liElement.length
+
+// console.log(howMany);
+
+
+
+
+// let liElement = document.getElementsByTagName('li');
+
+// let howMany = liElement.length
+
+
+// for(let i = 0; i < howMany; i++){
+//     if(liElement[i].innerHTML == ''){
+//         liElement[i].innerHTML = "Comming Soon";
+//     }
+// }
+
+
+
+// function checkWord(){
+//     let word = document.getElementById('word');
+//     word =  word.value
+//     console.log(word);
+// }
+
+
+// function checkWord () {
+//     let word = document.getElementById('word').value;
+//     let reserve = word.split('').reverse().join('');
+
+//     if(reserve == word){
+//         alert('Your Word is Plindrom')
+//     }else
+//     {
+//         alert('Please Try Again')
+//     }
+// }
+
+
+// let getInput = prompt('Enter your Word');
+
+// // console.log(getInput.length -1);
+
+
+// for (let i = getInput.length -1; i >= 0; i--) {
+//     let reverse = '';
+//     reverse += getInput
+//     console.log('Your Reverse Word is =>',reverse,i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
